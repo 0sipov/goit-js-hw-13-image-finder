10c463479db6dd514a0417342853df6a5eff04f2
Osipov Sergey <smozoluk@gmail.com>
2021-01-17 22:23:55 +0200

    add scroll

--- src/js/galleryHandler.js
+++ src/js/galleryHandler.js
@@ -17,18 +17,18 @@ function nextPageRequest(query) {
   page += 1;
   imgFetch(query, page).then((arr) => {
     refs.galleryRef.insertAdjacentHTML("beforeEnd", galleryTemplate(arr));
-    const lastChild = refs.galleryRef.lastElementChild.querySelector('img')
-
-    lastChild.onload = function () {
-      console.log(123)
-      window.scrollTo({
-        top: 300000,
-        behavior: "smooth",
-      });
-    };
-  })
+    const imgsRef = Array.from(refs.galleryRef.querySelectorAll("img"));
+    imgsRef.map((e) => {
+      e.onload = function () {
+        window.scrollTo({
+          top: document.documentElement.scrollHeight,
+          behavior: "smooth",
+        });
+      };
+    });
+  });
 }
 
-export {newRequest, nextPageRequest};
+export { newRequest, nextPageRequest };
 
 // document.documentElement.scrollHeight;
